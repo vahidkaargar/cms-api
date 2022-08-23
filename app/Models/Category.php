fecb0472a305c9256b43790986465da5d05d462b
Vahid Kaargar <vahidkaargar@gmail.com>
2022-08-23 16:18:47 +0430

    Add fillable fields to category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -10,6 +10,8 @@ class Category extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['title', 'slug', 'descriptions', 'tree'];
+
     public function posts(): HasMany
     {
         return $this->hasMany(Post::class);
