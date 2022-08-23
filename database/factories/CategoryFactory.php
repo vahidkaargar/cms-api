0e50af0931001ef3312e0ca952de3cfbb1ab9e8a
Vahid Kaargar <vahidkaargar@gmail.com>
2022-08-23 15:55:14 +0430

    Create post and category factory

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
+ */
+class CategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => fake()->realText(30),
+            'slug' => fake()->slug(),
+            'descriptions' => fake()->realText(100),
+        ];
+    }
+}
