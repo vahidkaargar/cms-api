af410b46243a7663faacf42663608ca4aef038d8
Vahid Kaargar <vahidkaargar@gmail.com>
2022-08-27 23:59:08 +0430

    add seeders to database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
+            AbilitySeeder::class,
+            AbilityUserSeeder::class,
         ]);
     }
 }
